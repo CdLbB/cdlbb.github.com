379f94776e711f51b91d91e9b0edb7ea8a423092
Eric Nitardy <nithardt@mac.com>
2014-03-20 19:32:18 -0700

    Added Trebuchet to title font stack, and change Javascript to adjust z-index when unhiding

--- WandW/js/showhide.js
+++ WandW/js/showhide.js
@@ -1,14 +1,14 @@
 (document.getElementById) ? dom = true : dom = false;
 function hideIt(layer) {
-if (dom) {document.getElementById(layer).style.visibility='hidden';}
+if (dom) {document.getElementById(layer).style.visibility='hidden';document.getElementById(layer).style.zIndex='30';}
  }
 function showIt(layer) {
-if (dom) {document.getElementById(layer).style.visibility='visible';}
+if (dom) {document.getElementById(layer).style.visibility='visible';document.getElementById(layer).style.zIndex='40';}
 }
 function toggleHide(layer){
 if (dom){
- if (document.getElementById(layer).style.visibility=='visible'){document.getElementById(layer).style.visibility='hidden';}
+ if (document.getElementById(layer).style.visibility=='visible'){document.getElementById(layer).style.visibility='hidden';document.getElementById(layer).style.zIndex='30';}
 else
-{document.getElementById(layer).style.visibility='visible';}
+{document.getElementById(layer).style.visibility='visible';document.getElementById(layer).style.zIndex='40';}
 }
 }
